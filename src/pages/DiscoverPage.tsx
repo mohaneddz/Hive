2e94a8b5b1a1098f105f1101c361c39a922c385e
Mohaned <akumaxd5@gmail.com>
2026-07-24 22:01:11 +0100

    refactor(gallery): remove DiscoverPage

--- src/pages/DiscoverPage.tsx
+++
@@ -1,8 +0,0 @@
-import { artworks } from "@/data/gallery";
-import { ArtworkGrid } from "@/sections/gallery/ArtworkGrid";
-import { DiscoverHeader } from "@/sections/gallery/DiscoverHeader";
-import { ExhibitionFeature } from "@/sections/gallery/ExhibitionFeature";
-
-export function DiscoverPage() {
-  return <div className="space-y-7"><DiscoverHeader /><ExhibitionFeature /><ArtworkGrid artworks={artworks.slice(0, 3)} /></div>;
-}
