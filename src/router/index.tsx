eacef8aebf26cacf3bdcf2b5244e845672556f95
Mohaned <akumaxd5@gmail.com>
2026-07-25 10:06:25 +0100

    feat(router): wire up the new pages added to the app shell

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,10 +1,17 @@
 import { createHashRouter } from "react-router-dom";
 
 import { AppShell } from "@/components/layout/AppShell";
+import { CollectionsPage } from "@/pages/CollectionsPage";
+import { ExplorerPage } from "@/pages/ExplorerPage";
 import { GalleryPage } from "@/pages/GalleryPage";
 import { GallerySettingsPage } from "@/pages/GallerySettingsPage";
+import { HomePage } from "@/pages/HomePage";
 import { NotFoundPage } from "@/pages/NotFoundPage";
+import { PeoplePage } from "@/pages/PeoplePage";
+import { PlacesPage } from "@/pages/PlacesPage";
 import { SearchPage } from "@/pages/SearchPage";
+import { TrashPage } from "@/pages/TrashPage";
+import { UtilitiesPage } from "@/pages/UtilitiesPage";
 import { ViewerPage } from "@/pages/ViewerPage";
 import { routes } from "@/config/routes";
 
@@ -12,8 +19,15 @@ export const router = createHashRouter([
   {
     element: <AppShell />,
     children: [
-      { index: true, element: <GalleryPage /> },
+      { index: true, element: <HomePage /> },
+      { path: routes.gallery.path, element: <GalleryPage /> },
+      { path: routes.collections.path, element: <CollectionsPage /> },
+      { path: routes.people.path, element: <PeoplePage /> },
+      { path: routes.places.path, element: <PlacesPage /> },
+      { path: routes.explorer.path, element: <ExplorerPage /> },
       { path: routes.search.path, element: <SearchPage /> },
+      { path: routes.utilities.path, element: <UtilitiesPage /> },
+      { path: routes.trash.path, element: <TrashPage /> },
       { path: "/media/:id", element: <ViewerPage /> },
       { path: routes.settings.path, element: <GallerySettingsPage /> },
       { path: "*", element: <NotFoundPage /> },
